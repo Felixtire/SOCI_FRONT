4a857c8675ada093e19cf5e554cad682374e49be
Wellington Felix <warlindo0@gmail.com>
2026-06-04 21:32:11 -0300

    dev

--- soci-front/src/app/app.routes.ts
+++ soci-front/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { TelaCadastro } from './telas/tela-cadastro/tela-cadastro';
+import { TelaLogin } from './telas/tela-login/tela-login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TelaCadastro
+  },
+
+  {
+    path: 'login',
+    component: TelaLogin
+  }
+
+
+];
--- soci-front/src/app/telas/tela-cadastro/tela-cadastro.ts
+++ soci-front/src/app/telas/tela-cadastro/tela-cadastro.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tela-cadastro',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './tela-cadastro.html',
-  styleUrl: './tela-cadastro.css',
+  styleUrls: ['./tela-cadastro.css'],
 })
 export class TelaCadastro {}
